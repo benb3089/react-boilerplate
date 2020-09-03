6fb85ddea25f0d5e172b6d6dc4d1d64e64e765ab
Ben B <benb@callboxinc.com>
2020-09-04 06:27:37 +0800

    Switch to exporting webpack config as function.
    - This allows passing of --env variables.
    - Fixes (unnoticed) bug with prod/dev builds settings that check
      process.env.NODE_ENV when it is not actually set. Set and use
      --env.NODE_ENV instead.

--- webpack.config.js
+++ webpack.config.js
@@ -1,126 +1,128 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 const reactVersion = require('react').version;
 const reactDOMVersion = require('react-dom').version;
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.module\.s[ac]ss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]',
+module.exports = (env) => {
+  const isDevelopment = env.NODE_ENV === 'development';
+
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.module\.s[ac]ss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]___[hash:base64:5]',
+                },
+                localsConvention: 'camelCase',
+                sourceMap: isDevelopment,
               },
-              localsConvention: 'camelCase',
-              sourceMap: isDevelopment,
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        exclude: /\.module\.s[ac]ss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.module\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
               },
-              localsConvention: 'camelCase',
-              sourceMap: isDevelopment,
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        exclude: /\.module\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/,
+          exclude: /\.module\.s[ac]ss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
             },
+          ],
+        },
+        {
+          test: /\.module\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]___[hash:base64:5]',
+                },
+                localsConvention: 'camelCase',
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: /\.module\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.scss'],
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].[hash].bundle.js',
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: './src/index.ejs',
+        cdn: {
+          use: 'unpkg',
+          unpkg: {
+            react: `https://unpkg.com/react@${reactVersion}/umd/react.development.js`,
+            reactDOM: `https://unpkg.com/react-dom@${reactDOMVersion}/umd/react-dom.development.js`,
+            domain: '//unpkg.com/',
           },
-        ],
-      },
+          cdnjs: {
+            react: `https://cdnjs.cloudflare.com/ajax/libs/react/${reactVersion}/umd/react.development.js`,
+            reactDOM: `https://cdnjs.cloudflare.com/ajax/libs/react-dom/${reactDOMVersion}/umd/react-dom.development.min.js`,
+            domain: '//cdnjs.cloudflare.com/',
+          },
+          jsdelivr: {
+            react: `https://cdn.jsdelivr.net/npm/react@${reactVersion}/umd/react.development.js`,
+            reactDOM: `https://cdn.jsdelivr.net/npm/react-dom@${reactDOMVersion}/umd/react-dom.development.js`,
+            domain: '//cdn.jsdelivr.net/',
+          },
+        },
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.scss'],
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[hash].bundle.js',
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: './src/index.ejs',
-      cdn: {
-        use: 'unpkg',
-        unpkg: {
-          react: `https://unpkg.com/react@${reactVersion}/umd/react.development.js`,
-          reactDOM: `https://unpkg.com/react-dom@${reactDOMVersion}/umd/react-dom.development.js`,
-          domain: '//unpkg.com/',
-        },
-        cdnjs: {
-          react: `https://cdnjs.cloudflare.com/ajax/libs/react/${reactVersion}/umd/react.development.js`,
-          reactDOM: `https://cdnjs.cloudflare.com/ajax/libs/react-dom/${reactDOMVersion}/umd/react-dom.development.min.js`,
-          domain: '//cdnjs.cloudflare.com/',
-        },
-        jsdelivr: {
-          react: `https://cdn.jsdelivr.net/npm/react@${reactVersion}/umd/react.development.js`,
-          reactDOM: `https://cdn.jsdelivr.net/npm/react-dom@${reactDOMVersion}/umd/react-dom.development.js`,
-          domain: '//cdn.jsdelivr.net/',
-        },
-      },
-    }),
-  ],
-  devServer: {
-    contentBase: './dist',
-  },
+    devServer: {
+      contentBase: './dist',
+    },
+  };
 };
